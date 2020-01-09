7d66d99f0d6570b6973e58a1d7a830bc5f64ae04
ashleyxz <59430894+ashleyxyz@users.noreply.github.com>
2020-01-08 20:17:43 -0800

    Initical Commit

---
+++ Unit_1/U1_L2_Q6Udacity.py
@@ -0,0 +1,16 @@
+# Quiz 6
+# Given the variables s and t defined as:
+s = 'udacity'
+t = 'bodacious'
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+
+print s[0:3]+t[4:]
+
+print s[:5]+t[6:]
+
+# Second Solution
+print s[:-2]
+print t[-3:]
+print s[:-2]+t[-3:]
